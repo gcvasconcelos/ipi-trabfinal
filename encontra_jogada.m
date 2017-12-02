ecfbfc7134e58f3fcd3148862a5f8f722a4282b7
Gabriel Vasconcelos <gcvasconcelos98@gmail.com>
2017-12-02 14:08:44 -0200

    Desenvolvendo o código

--- encontra_jogada.m
+++ encontra_jogada.m
@@ -12,17 +12,17 @@ H = vision.TemplateMatcher;
 %encontra template na imagem
 LOC = step(H,I,T);
 
-%é possivel especificar o quadrante
+%ï¿½ possivel especificar o quadrante
 %[linha, coluna] = size(I);
 %LOC = step(H,I,T, [1 1 coluna linha]);
 
-imshow(I);
+figure, imshow(I);
 hold on 
 plot(LOC(1),LOC(2),'x','LineWidth',2,'Color','red');
 
 %Encontra centro do circulo e raio
 %seria melhor utilizadar um template? parte mais demorada
-[center, raddi] = imfindcircles(I, [15 30]);
-
-viscircles(center,raddi,'EdgeColor','b');
-hold off
+% [center, raddi] = imfindcircles(I, [15 30]);
+% 
+% viscircles(center,raddi,'EdgeColor','b');
+% hold off
--- projfinal.m
+++ projfinal.m
@@ -1,49 +1,79 @@
 close all
+clear all
 clc
 
 % cam = webcam;
 % preview(cam);
 % snapshot(cam);
 
-img = imread('melhorcaso.jpg');
+img = imread('teste1.jpg');
 
 img = rgb2gray(img);
-BW = imbinarize(img);
-bordas = edge(BW,'canny');
 
-[H,theta,rho] = hough(bordas,'RhoResolution',0.5,'ThetaResolution',0.5);
-peaks = houghpeaks(H,8);
-linhas = houghlines(BW,theta,rho,peaks);
+borders = edge(img,'canny', 0.5);
 
-figure
+imshow(borders);
+
+
+[H,theta,rho] = hough(borders);
+peaks = houghpeaks(H,4);
+lines = houghlines(img,theta,rho,peaks,'FillGap',100,'MinLength',50);
+
+figure(1)
 imshow(imadjust(rescale(H)),'XData',theta,'YData',rho, 'InitialMagnification','fit');
 title('Transformada de Hough no tabuleiro do jogo da velha');
 xlabel('\theta'), ylabel('\rho');
-axis on, axis normal, hold on;
-colormap(gca,hot);
 
-peaks  = houghpeaks(H,8);
-x = theta(peaks(:,2)); y = rho(peaks(:,1));
-plot(x,y,'s','color','white');
+axis on, axis normal, hold on
 
-lines = houghlines(BW,theta,rho,peaks,'FillGap',5,'MinLength',7);
-figure, imshow(img), hold on;
-max_len = 0;
+x = theta(peaks(:,2));
+y = rho(peaks(:,1));
+plot(x,y,'s','color','red');
+
+hold off
+
+figure(2), imshow(img);
+hold on
+
 for k = 1:length(lines)
    xy = [lines(k).point1; lines(k).point2];
    plot(xy(:,1),xy(:,2),'LineWidth',2,'Color','green');
 
    % Plot beginnings and ends of lines
-   plot(xy(1,1),xy(1,2),'x','LineWidth',2,'Color','yellow');
-   plot(xy(2,1),xy(2,2),'x','LineWidth',2,'Color','red');
-
-   % Determine the endpoints of the longest line segment
-   len = norm(lines(k).point1 - lines(k).point2);
-   if ( len > max_len)
-      max_len = len;
-      xy_long = xy;
-   end
+   plot(xy(1,1),xy(1,2),'x','LineWidth',5,'Color','red');
+   plot(xy(2,1),xy(2,2),'x','LineWidth',5,'Color','red');
 end
 
-hold off;
+hold off
+
+img_size = size(img);
+lil_board_size  = [lines(2).point1(1)-lines(3).point1(1),lines(4).point1(2)-lines(1).point1(2)];
+board_size = [3*lil_board_size(1), 3*lil_board_size(2)];
+pad_size = floor((img_size - board_size)/2);
+
+board_block = img(pad_size(1):img_size(1)-pad_size(1), pad_size(2):img_size(2)-pad_size(2));
+board_block_size = size(board_block);
+
+board_state = [0 0 0; 0 0 0; 0 0 0];
+
+% play = imread('melhorx.jpg');
+% play = rgb2gray(play);
+x_template = imread('X_rots.jpg');
+x_template = rgb2gray(x_template);
+H = vision.TemplateMatcher;
+
+LOC = step(H,board_block,x_template);
+% [line, column] = size(board_block);
+% LOC = step(H,play,x_template, [1 1 column line]);
+
+figure, imshow(board_block);
+hold on 
+plot(LOC(1),LOC(2),'x','LineWidth',2,'Color','red');
+
+%Encontra centro do circulo e raio
+%seria melhor utilizadar um template? parte mais demorada
+% [center, raddi] = imfindcircles(I, [15 30]);
+% 
+% viscircles(center,raddi,'EdgeColor','b');
+% hold off
 
