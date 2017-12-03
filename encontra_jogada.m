0e9fd1c029bcf0d6bbf4ebcea075936d30a8fbed
Gabriel Vasconcelos <gcvasconcelos98@gmail.com>
2017-12-02 23:32:13 -0200

    adicionando as funções no codigo central

--- encontra_jogada.m
+++ encontra_jogada.m
@@ -26,3 +26,31 @@ plot(LOC(1),LOC(2),'x','LineWidth',2,'Color','red');
 % 
 % viscircles(center,raddi,'EdgeColor','b');
 % hold off
+
+% borders = edge(img,'canny', 0.5);
+% [H,theta,rho] = hough(borders);
+% peaks = houghpeaks(H,5);
+% lines = houghlines(img,theta,rho,peaks);
+% 
+% figure(1), imshow(imadjust(rescale(H)),'XData',theta,'YData',rho, 'InitialMagnification','fit');
+% title('Transformada de Hough no tabuleiro do jogo da velha');
+% xlabel('\theta'), ylabel('\rho');
+% 
+% axis on, axis normal, hold on
+% 
+% x = theta(peaks(:,2));
+% y = rho(peaks(:,1));
+% plot(x,y,'s','color','red');
+% hold off
+% 
+% figure(2), imshow(img);
+% hold on
+% for k = 1:length(lines)
+%    xy = [lines(k).point1; lines(k).point2];
+%    plot(xy(:,1),xy(:,2),'LineWidth',2,'Color','green');
+% 
+%    plot(xy(1,1),xy(1,2),'x','LineWidth',5,'Color','red');
+%    plot(xy(2,1),xy(2,2),'x','LineWidth',5,'Color','red');
+% end
+% 
+% hold off
