c6d1ca5762c60578b34abe82f55b4cc61f1680f0
ACSouzaJr <junioracs2000@hotmail.com>
2017-12-03 17:38:14 -0200

    arruma condicoes de vitoria

--- projfinal.m
+++ projfinal.m
@@ -8,11 +8,12 @@ preview(cam);
 board = zeros(3, 3);
 frame = getSnapshot(cam);
 
+result = won(board);
 while result == -1
     lastframe = frame;
     frame = getSnapshot(cam);
     frame = getBoard(frame);
-    
+
     pause;
     diff = psnr(frame, lastframe);
     if (diff < 22)
@@ -22,11 +23,7 @@ while result == -1
     end
 end
 
-
-% img = openImage('teste2.jpg');
-% img2 = openImage('teste3.jpg');
-% img = getBoard(img);
-% img2 = getBoard(img2);
+checkWin(result);
 
 % captura o frame atual da camera e formata para analise
 function tmp = getSnapshot(cam)
@@ -43,7 +40,7 @@ function img = openImage(name)
 end
 
 % segmenta a imagem utilizando os pontos de Harrris
-% retorna apenas a parte da imagem com o tabuleiro 
+% retorna apenas a parte da imagem com o tabuleiro
 function board = getBoard(img)
     corners = detectHarrisFeatures(img);
     x_c = floor(abs(corners.Location(:,2)));
@@ -63,7 +60,7 @@ function [] = playerTurn(frame, nextframe, board)
     elseif (isempty(center))
         return;
     end
-    
+
     x = center(1); y = center(2);
     board_size = size(nextframe);
     cell_x = board_size(1)/3; cell_y = board_size(2)/3;
@@ -93,30 +90,30 @@ end
 
 % jogada do PC
 function [] = computerTurn(frame, board, player)
-    %frame = frame atual
-    %board = board sendo, 0 as posicoes vazias
-    %player = a opção de jogada do player
-    %assim que o matlab lida com matrix
+    % frame = frame atual
+    % board = board sendo, 0 as posicoes vazias
+    % player = a opção de jogada do player
+    % assim que o matlab lida com matrix
     % |1| |4|  |7|
     % |2| |5|  |8|
     % |3| |6|  |9|
-    
-    %devide o board em regioes
-    [h,w] = size(frame);
-    
-    A = [1 1; 1 w/3; 1 2*w/3;...
-    h/3 1; h/3 w/3; h/3 2*w/3;...
-    2*h/3 1; 2*h/3 w/3; 2*h/3 2*h/3];
 
-    %supondo que espaços vazio sao 0
-    %procura pela primeira posicao vazia
-    %acha posicao aleatoria vazia
-    pc = find(~board);
+    % devide o board em regioes
+    [h,w] = size(frame);
+
+    A = [1 1; 1 w/3; 1 2*w/3;...
+        h/3 1; h/3 w/3; h/3 2*w/3;...
+        2*h/3 1; 2*h/3 w/3; 2*h/3 2*h/3];
+
+    % supondo que espaços vazio sao 0
+    % procura pela primeira posicao vazia
+    % acha posicao aleatoria vazia
+    pc = find(board==0);
     pc = pc(randperm(length(pc)));
     pc = pc(1);
-    
 
-    board(pc) = 2;%marca jogada do pc
+    % marca jogada do pc
+    board(pc) = 2;
 
     row = (A(pc,2) + w/6);
     col = (A(pc,1) + h/6);
@@ -140,14 +137,13 @@ function [] = computerTurn(frame, board, player)
         neg = 1-x;
         plot(x+col, pos+row,'LineWidth',60,'Color','red')
         plot(x+col, neg+row,'LineWidth',60,'Color','red')
-     hold off
+        hold off
     end
-
 end
 
 % analisa o board e detecta se alguem venceu
 % retorna a 1 se o 'Jogador' venceu
-%           2 se o 'Computador' venceu        
+%           2 se o 'Computador' venceu
 %           0 se deu velha
 %          -1 se nada aconteceu
 function result = won(board)
@@ -159,11 +155,11 @@ function result = won(board)
     elseif (board(3,1) == board(3,2) && board(3,1) == board(3,3) && board(3,1) ~= 0)
         result = board(3,1);
     % vertical
-    elseif (board(1,1) == board(2,1) && board(1,1) == board(3,1) && board(3,1) ~= 0) 
+    elseif (board(1,1) == board(2,1) && board(1,1) == board(3,1) && board(3,1) ~= 0)
         result = board(1,1);
-    elseif (board(1,2) == board(2,2) && board(1,2) == board(3,2) && board(1,2) ~= 0) 
+    elseif (board(1,2) == board(2,2) && board(1,2) == board(3,2) && board(1,2) ~= 0)
         result = board(1,2);
-    elseif (board(1,3) == board(2,3) && board(1,3) == board(3,3) && board(1,3) ~= 0) 
+    elseif (board(1,3) == board(2,3) && board(1,3) == board(3,3) && board(1,3) ~= 0)
         result = board(1,3);
     % diagonal
     elseif (board(1,1) == board(2,2) && board(1,1) == board(3,3) && board(1,1) ~= 0)
@@ -177,15 +173,15 @@ function result = won(board)
     end
 end
 
-function checkWin(board)
+function [] = checkWin(result)
 
-    state = won(board);
-    if (state == 0)
-        disp('Ninguém ganhou!')
-    elseif state == 1
-        disp('O Jogador Ganhou')
-    elseif state == 2
-        disp('O Computador Ganhou')
+    if (result == 0)
+        warndlg('Ninguém ganhou!')
+    elseif result == 1
+        warndlg('O Jogador Ganhou')
+    elseif result == 2
+        warndlg('O Computador Ganhou')
+    else
+        warndlg('Ocorreu algum erro')
     end
-        
 end
